d107bc5f6e5615dfde376769ea0224870fcb30dc
Alexander Ecker <alex@deepart.io>
2017-04-12 16:13:17 +0200

    Added in/out smoothing and upsampling of input

--- webcam_demo.lua
+++ webcam_demo.lua
@@ -17,7 +17,7 @@ local preprocess = require 'fast_neural_style.preprocess'
 local cmd = torch.CmdLine()
 
 -- Model options
-cmd:option('-models', 'models/instance_norm/candy.t7')
+cmd:option('-models', 'models/models/instance_norm/candy.t7')
 cmd:option('-height', 480)
 cmd:option('-width', 640)
 
@@ -29,6 +29,8 @@ cmd:option('-use_cudnn', 1)
 -- Webcam options
 cmd:option('-webcam_idx', 0)
 cmd:option('-webcam_fps', 60)
+cmd:option('-alpha_in', 0.1, 'smoothing parameter for input frames')
+cmd:option('-alpha_out', 0.1, 'smoothing parameter for output frames')
 
 
 local function main()
@@ -42,6 +44,10 @@ local function main()
     print('loading model from ', checkpoint_path)
     local checkpoint = torch.load(checkpoint_path)
     local model = checkpoint.model
+    -- insert bilinear upsampling into the model
+    local upsamp = nn.SpatialUpSamplingBilinear(2)
+    model:insert(upsamp, 1)
+    -- model:insert(upsamp, #model + 1)
     model:evaluate()
     model:type(dtype)
     if use_cudnn then
@@ -71,23 +77,37 @@ local function main()
   local cam = image.Camera(camera_opt)
 
   local win = nil
+  local img_pre = nil
+  local imgs_out = {}
   while true do
     -- Grab a frame from the webcam
     local img = cam:forward()
-
+    img = image.hflip(img)
     -- Preprocess the frame
+    local alpha_in = opt.alpha_in
     local H, W = img:size(2), img:size(3)
     img = img:view(1, 3, H, W)
-    local img_pre = preprocess.preprocess(img):type(dtype)
+    if img_pre then
+      img_pre = (alpha_in * img_pre + (1-alpha_in) * preprocess.preprocess(img):type(dtype))
+    else
+      img_pre = preprocess.preprocess(img):type(dtype)
+    end
 
     -- Run the models
-    local imgs_out = {}
     for i, model in ipairs(models) do
+      --local timer22 = torch.Timer() --Jonas
       local img_out_pre = model:forward(img_pre)
+      --if cutorch then cutorch.synchronize() end --Jonas
+      --print(timer22:time().real) --Jonas
 
       -- Deprocess the frame and show the image
+      local alpha_out = opt.alpha_out
       local img_out = preprocess.deprocess(img_out_pre)[1]:float()
-      table.insert(imgs_out, img_out)
+      if imgs_out[i] then
+        imgs_out[i] = (alpha_out * imgs_out[i] + (1-alpha_out) * img_out)
+      else
+        table.insert(imgs_out, img_out)
+      end
     end
     local img_disp = image.toDisplayTensor{
       input = imgs_out,
